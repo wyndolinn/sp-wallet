65642d55e3f1ea78bbc2a7e086546e38acf0cfa1
Maxim Kharebin <maxim.kharebin@gmail.com>
2025-06-01 20:19:41 +0300

    Update typography

--- sharedCore/src/commonMain/kotlin/com/wynndie/spwallet/sharedCore/presentation/theme/Typography.kt
+++ sharedCore/src/commonMain/kotlin/com/wynndie/spwallet/sharedCore/presentation/theme/Typography.kt
@@ -1,7 +1,123 @@
 package com.wynndie.spwallet.sharedCore.presentation.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 
 val Typography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontWeight = FontWeight(600),
+        fontSize = 36.sp,
+        lineHeight = 42.sp,
+        letterSpacing = 0.0.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontWeight = FontWeight(400),
+        fontSize = 36.sp,
+        lineHeight = 42.sp,
+        letterSpacing = 0.0.sp
+    ),
+    displaySmall = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontWeight = FontWeight(300),
+        fontSize = 36.sp,
+        lineHeight = 42.sp,
+        letterSpacing = 0.0.sp
+    ),
 
+
+    headlineLarge = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontWeight = FontWeight(600),
+        fontSize = 24.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.0.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontWeight = FontWeight(400),
+        fontSize = 24.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.0.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontWeight = FontWeight(300),
+        fontSize = 24.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.0.sp
+    ),
+
+
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontWeight = FontWeight(600),
+        fontSize = 16.sp,
+        lineHeight = 19.sp,
+        letterSpacing = 0.0.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontWeight = FontWeight(400),
+        fontSize = 16.sp,
+        lineHeight = 19.sp,
+        letterSpacing = 0.0.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontWeight = FontWeight(300),
+        fontSize = 16.sp,
+        lineHeight = 19.sp,
+        letterSpacing = 0.0.sp
+    ),
+
+
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontWeight = FontWeight(600),
+        fontSize = 14.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.0.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontWeight = FontWeight(400),
+        fontSize = 14.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.0.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontWeight = FontWeight(300),
+        fontSize = 14.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.0.sp
+    ),
+
+
+    labelLarge = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontWeight = FontWeight(600),
+        fontSize = 12.sp,
+        lineHeight = 14.sp,
+        letterSpacing = 0.0.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontWeight = FontWeight(400),
+        fontSize = 12.sp,
+        lineHeight = 14.sp,
+        letterSpacing = 0.0.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.SansSerif,
+        fontWeight = FontWeight(300),
+        fontSize = 12.sp,
+        lineHeight = 14.sp,
+        letterSpacing = 0.0.sp
+    )
 )
