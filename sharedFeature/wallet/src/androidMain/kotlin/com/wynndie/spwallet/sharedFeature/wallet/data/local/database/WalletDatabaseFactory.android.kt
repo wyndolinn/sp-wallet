4e91b66e80da9ac6b62d73bfd1b25e41de154fbd
m.kharebin <m.kharebin@sendy.land>
2025-06-20 15:17:58 +0300

    Suppress expect/actual classes

--- sharedFeature/wallet/src/androidMain/kotlin/com/wynndie/spwallet/sharedFeature/wallet/data/local/database/WalletDatabaseFactory.android.kt
+++ sharedFeature/wallet/src/androidMain/kotlin/com/wynndie/spwallet/sharedFeature/wallet/data/local/database/WalletDatabaseFactory.android.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 actual class WalletDatabaseFactory(
     private val context: Context
 ) {
