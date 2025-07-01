06d46f5efefc1e84392ded22ef273b69ae6f7cd8
Maxim Kharebin <maxim.kharebin@gmail.com>
2025-07-01 21:20:04 +0300

    Swap singleOf with viewModelOf

--- composeApp/src/commonMain/kotlin/com/wynndie/spwallet/di/SharedModule.kt
+++ composeApp/src/commonMain/kotlin/com/wynndie/spwallet/di/SharedModule.kt
@@ -2,12 +2,12 @@ package com.wynndie.spwallet.di
 
 import com.wynndie.spwallet.AppViewModel
 import com.wynndie.spwallet.sharedFeature.wallet.di.walletSharedModule
-import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val appSharedModule = module {
 
-    singleOf(::AppViewModel)
+    viewModelOf(::AppViewModel)
 
     includes(
         walletSharedModule
