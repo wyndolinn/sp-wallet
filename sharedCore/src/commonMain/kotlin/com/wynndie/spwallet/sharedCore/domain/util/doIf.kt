842f4c74276ec59f688ea4f336be56c699ffc964
m.kharebin <m.kharebin@sendy.land>
2025-08-25 14:05:59 +0300

    Remove Any extension from .doIf()

--- sharedCore/src/commonMain/kotlin/com/wynndie/spwallet/sharedCore/domain/util/doIf.kt
+++ sharedCore/src/commonMain/kotlin/com/wynndie/spwallet/sharedCore/domain/util/doIf.kt
@@ -4,6 +4,6 @@ inline fun <T> T.doIf(predicate: Boolean, action: T.() -> T): T {
     return if (predicate) action(this) else this
 }
 
-inline fun <T, R : Any> T.doIfNotNull(value: R?, action: T.(R) -> T): T {
+inline fun <T, R> T.doIfNotNull(value: R?, action: T.(R) -> T): T {
     return if (value != null) action(this, value) else this
 }
