fdab347a757123fd5659e0a3853b2677970bf4c3
Maxim Kharebin <maxim.kharebin@gmail.com>
2025-09-25 19:39:32 +0300

    Return specific painter from UiImage.asPainter()

--- sharedCore/src/commonMain/kotlin/com/wynndie/spwallet/sharedCore/presentation/formatters/UiImage.kt
+++ sharedCore/src/commonMain/kotlin/com/wynndie/spwallet/sharedCore/presentation/formatters/UiImage.kt
@@ -3,23 +3,39 @@ package com.wynndie.spwallet.sharedCore.presentation.formatters
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.VectorPainter
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import coil3.compose.AsyncImagePainter
 import coil3.compose.rememberAsyncImagePainter
 import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.painterResource
 
 sealed interface UiImage {
 
-    data class Drawable(val id: DrawableResource) : UiImage
-    data class Vector(val vector: ImageVector) : UiImage
-    data class Url(val value: String) : UiImage
-
     @Composable
-    fun asPainter(): Painter {
-        return when (this) {
-            is Drawable -> painterResource(id)
-            is Vector -> rememberVectorPainter(vector)
-            is Url -> rememberAsyncImagePainter(value)
+    fun asPainter(): Painter
+
+    data class Drawable(val id: DrawableResource) : UiImage {
+
+        @Composable
+        override fun asPainter(): Painter {
+            return painterResource(id)
+        }
+    }
+
+    data class Vector(val vector: ImageVector) : UiImage {
+
+        @Composable
+        override fun asPainter(): VectorPainter {
+            return rememberVectorPainter(vector)
+        }
+    }
+
+    data class Url(val value: String) : UiImage {
+
+        @Composable
+        override fun asPainter(): AsyncImagePainter {
+            return rememberAsyncImagePainter(value)
         }
     }
 }
