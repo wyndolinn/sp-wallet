a51d7c573db3384c5ded9a54dc3fea1b210af325
Maxim Kharebin <maxim.kharebin@gmail.com>
2025-09-28 17:32:45 +0300

    Add InputField

---
+++ sharedCore/src/commonMain/kotlin/com/wynndie/spwallet/sharedCore/presentation/components/input/BasicInputField.kt
@@ -0,0 +1,106 @@
+package com.wynndie.spwallet.sharedCore.presentation.components.input
+
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.OutlinedTextFieldDefaults.Container
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import com.wynndie.spwallet.sharedtheme.theme.spacing
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BasicInputField(
+    value: TextFieldValue,
+    onValueChange: (TextFieldValue) -> Unit,
+    modifier: Modifier = Modifier,
+    placeholder: String? = null,
+    label: @Composable (() -> Unit)? = null,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    trailingIcon: @Composable (() -> Unit)? = null,
+    prefix: @Composable (() -> Unit)? = null,
+    suffix: @Composable (() -> Unit)? = null,
+    supportingText: @Composable (() -> Unit)? = null,
+    hasError: Boolean = false,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    singleLine: Boolean = true,
+    minLines: Int = 1,
+    maxLines: Int = Int.MAX_VALUE,
+    readOnly: Boolean = false,
+    enabled: Boolean = true
+) {
+
+    val colors = OutlinedTextFieldDefaults.colors()
+
+
+    BasicTextField(
+        value = value,
+        onValueChange = onValueChange,
+        enabled = enabled,
+        readOnly = readOnly,
+        textStyle = MaterialTheme.typography.bodyMedium,
+        keyboardOptions = keyboardOptions,
+        keyboardActions = keyboardActions,
+        singleLine = singleLine,
+        maxLines = maxLines,
+        minLines = minLines,
+        visualTransformation = visualTransformation,
+        interactionSource = interactionSource,
+        decorationBox = @Composable { innerTextField ->
+            OutlinedTextFieldDefaults.DecorationBox(
+                value = value.text,
+                visualTransformation = visualTransformation,
+                innerTextField = innerTextField,
+                placeholder = placeholder?.let {
+                    {
+                        Text(
+                            text = it,
+                            style = MaterialTheme.typography.bodyMedium,
+                            color = MaterialTheme.colorScheme.outline
+                        )
+                    }
+                },
+                label = label,
+                leadingIcon = leadingIcon,
+                trailingIcon = trailingIcon,
+                prefix = prefix,
+                suffix = suffix,
+                supportingText = supportingText,
+                singleLine = singleLine,
+                enabled = enabled,
+                isError = hasError,
+                interactionSource = interactionSource,
+                colors = colors,
+                container = {
+                    Container(
+                        enabled = enabled,
+                        isError = hasError,
+                        interactionSource = interactionSource,
+                        colors = colors,
+                        shape = MaterialTheme.shapes.medium,
+                        focusedBorderThickness = 2.dp,
+                        unfocusedBorderThickness = 2.dp,
+                    )
+                },
+                contentPadding = PaddingValues(
+                    horizontal = MaterialTheme.spacing.medium,
+                    vertical = MaterialTheme.spacing.small
+                )
+            )
+        },
+        modifier = modifier,
+    )
+}
