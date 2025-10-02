3ec8c32d825cbfd2c78418229495c01008221927
m.kharebin <m.kharebin@sendy.land>
2025-10-02 15:13:13 +0300

    Add navigation cooldown

--- sharedCore/src/commonMain/kotlin/com/wynndie/spwallet/sharedCore/presentation/controllers/navigation/NavController.kt
+++ sharedCore/src/commonMain/kotlin/com/wynndie/spwallet/sharedCore/presentation/controllers/navigation/NavController.kt
@@ -1,14 +1,43 @@
 package com.wynndie.spwallet.sharedCore.presentation.controllers.navigation
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 
 object NavController {
 
     private val _navEvent = Channel<NavEvent>()
     val navEvent = _navEvent.receiveAsFlow()
 
-    suspend fun navigate(navEvent: NavEvent) {
-        _navEvent.send(navEvent)
+    private var isNavigationLocked = false
+    private var cooldownJob: Job? = null
+
+    private const val NAVIGATION_COOLDOWN = 300L
+
+    suspend fun navigate(navEvent: NavEvent, ignoreCooldown: Boolean = false) {
+
+        if (ignoreCooldown) {
+            _navEvent.send(navEvent)
+            return
+        }
+
+        if (!isNavigationLocked) {
+            _navEvent.send(navEvent)
+            restartCooldown()
+        }
+    }
+
+    private fun restartCooldown() {
+        isNavigationLocked = true
+        cooldownJob?.cancel()
+        cooldownJob = CoroutineScope(Dispatchers.Default).launch {
+            delay(NAVIGATION_COOLDOWN)
+            isNavigationLocked = false
+        }
     }
 }
