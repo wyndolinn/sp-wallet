7575e02457b3620e96cc3bada908127a4c3ec6d0
Maxim Harebin <wyndolinnn@gmail.com>
2026-05-24 18:03:28 +0300

    refactor: change app colors and typography

--- sharedCore/src/commonMain/kotlin/com/wynndie/spwallet/sharedCore/presentation/extensions/asColor.kt
+++ sharedCore/src/commonMain/kotlin/com/wynndie/spwallet/sharedCore/presentation/extensions/asColor.kt
@@ -5,13 +5,13 @@ import com.wynndie.spwallet.sharedCore.domain.models.cards.CardColors
 
 fun CardColors.asColor(): Color {
     return when (this) {
-        CardColors.BLUE -> Color(0xFF2852C7)
-        CardColors.PURPLE -> Color(0xFF9528C7)
-        CardColors.PINK -> Color(0xFFC7286D)
-        CardColors.RED -> Color(0xFFC72828)
-        CardColors.ORANGE -> Color(0xFFC77528)
-        CardColors.YELLOW -> Color(0xFFC7B728)
-        CardColors.GREEN -> Color(0xFF499F49)
-        CardColors.TEAL -> Color(0xFF2884C7)
+        CardColors.BLUE -> Color(0xFF5968C7)
+        CardColors.PURPLE -> Color(0xFFBE59C7)
+        CardColors.PINK -> Color(0xFFC75992)
+        CardColors.RED -> Color(0xFFC7595B)
+        CardColors.ORANGE -> Color(0xFFC77A59)
+        CardColors.YELLOW -> Color(0xFFC7C359)
+        CardColors.GREEN -> Color(0xFF59C76A)
+        CardColors.TEAL -> Color(0xFF59A6C7)
     }
 }
--- sharedCore/src/commonMain/kotlin/com/wynndie/spwallet/sharedCore/presentation/theme/Typography.kt
+++ sharedCore/src/commonMain/kotlin/com/wynndie/spwallet/sharedCore/presentation/theme/Typography.kt
@@ -7,31 +7,31 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.wynndie.spwallet.sharedCore.Res
-import com.wynndie.spwallet.sharedCore.noto_sans_bold
-import com.wynndie.spwallet.sharedCore.noto_sans_medium
-import com.wynndie.spwallet.sharedCore.noto_sans_regular
-import com.wynndie.spwallet.sharedCore.noto_sans_semi_bold
+import com.wynndie.spwallet.sharedCore.roboto_bold
+import com.wynndie.spwallet.sharedCore.roboto_medium
+import com.wynndie.spwallet.sharedCore.roboto_regular
+import com.wynndie.spwallet.sharedCore.roboto_semi_bold
 import com.wynndie.spwallet.sharedCore.ubuntu_bold
 import com.wynndie.spwallet.sharedCore.ubuntu_medium
 import com.wynndie.spwallet.sharedCore.ubuntu_regular
 import org.jetbrains.compose.resources.Font
 
-val NotoSansFontFamily
+val RobotoFontFamily
     @Composable get() = FontFamily(
         Font(
-            resource = Res.font.noto_sans_regular,
+            resource = Res.font.roboto_regular,
             weight = FontWeight.Normal
         ),
         Font(
-            resource = Res.font.noto_sans_medium,
+            resource = Res.font.roboto_medium,
             weight = FontWeight.Medium
         ),
         Font(
-            resource = Res.font.noto_sans_semi_bold,
+            resource = Res.font.roboto_semi_bold,
             weight = FontWeight.SemiBold
         ),
         Font(
-            resource = Res.font.noto_sans_bold,
+            resource = Res.font.roboto_bold,
             weight = FontWeight.Bold
         )
     )
@@ -72,46 +72,46 @@ val Typography
 
 
         titleLarge = TextStyle(
-            fontFamily = NotoSansFontFamily,
+            fontFamily = RobotoFontFamily,
             fontWeight = FontWeight.Bold,
             fontSize = 18.sp
         ),
         titleMedium = TextStyle(
-            fontFamily = NotoSansFontFamily,
+            fontFamily = RobotoFontFamily,
             fontWeight = FontWeight.Medium,
             fontSize = 18.sp,
         ),
         titleSmall = TextStyle(
-            fontFamily = NotoSansFontFamily,
+            fontFamily = RobotoFontFamily,
             fontWeight = FontWeight.Medium,
             fontSize = 16.sp
         ),
 
 
         bodyMedium = TextStyle(
-            fontFamily = NotoSansFontFamily,
+            fontFamily = RobotoFontFamily,
             fontWeight = FontWeight.Normal,
             fontSize = 16.sp
         ),
         bodySmall = TextStyle(
-            fontFamily = NotoSansFontFamily,
+            fontFamily = RobotoFontFamily,
             fontWeight = FontWeight.Normal,
             fontSize = 14.sp
         ),
 
 
         labelLarge = TextStyle(
-            fontFamily = NotoSansFontFamily,
+            fontFamily = RobotoFontFamily,
             fontWeight = FontWeight.SemiBold,
             fontSize = 14.sp
         ),
         labelMedium = TextStyle(
-            fontFamily = NotoSansFontFamily,
+            fontFamily = RobotoFontFamily,
             fontWeight = FontWeight.Medium,
             fontSize = 12.sp
         ),
         labelSmall = TextStyle(
-            fontFamily = NotoSansFontFamily,
+            fontFamily = RobotoFontFamily,
             fontWeight = FontWeight.Normal,
             fontSize = 12.sp
         )
