9090996e9593df023e1c4a1166b2b78dc8af0bd7
Maxim Harebin <wyndolinnn@gmail.com>
2026-05-24 18:12:00 +0300

    fix: delete user data when all cards were deleted

--- sharedFeatures/home/src/commonMain/kotlin/com/wynndie/spwallet/sharedFeature/home/domain/useCases/DeleteAuthedCardUseCase.kt
+++ sharedFeatures/home/src/commonMain/kotlin/com/wynndie/spwallet/sharedFeature/home/domain/useCases/DeleteAuthedCardUseCase.kt
@@ -3,10 +3,12 @@ package com.wynndie.spwallet.sharedFeature.home.domain.useCases
 import com.wynndie.spwallet.sharedCore.domain.models.cards.AuthedCard
 import com.wynndie.spwallet.sharedCore.domain.repositories.CardsRepository
 import com.wynndie.spwallet.sharedCore.domain.repositories.PreferencesRepository
+import com.wynndie.spwallet.sharedCore.domain.repositories.UserRepository
 import kotlinx.coroutines.flow.first
 
 class DeleteAuthedCardUseCase(
     private val cardsRepository: CardsRepository,
+    private val userRepository: UserRepository,
     private val preferencesRepository: PreferencesRepository
 ) {
 
@@ -18,6 +20,10 @@ class DeleteAuthedCardUseCase(
 
         val server = preferencesRepository.getSelectedSpServer().first()
         val unauthedCards = cardsRepository.getUnauthedCards().first()
+        val users = userRepository.getAuthedUsers().first()
+        users.forEach {
+            if (it.server == server) userRepository.deleteAuthedUser(it)
+        }
         unauthedCards.forEach {
             if (it.server == server) cardsRepository.deleteUnauthedCard(it)
         }
