d9328ba577597f2bfb84a55c956fc47eb8bddf88
Maxim Harebin <wyndolinnn@gmail.com>
2026-08-16 13:43:07 +0300

    refactor: rename rawText to originalText

--- sharedCore/src/commonMain/kotlin/com/wynndie/spwallet/sharedCore/presentation/visualTransformations/AmountVisualTransformation.kt
+++ sharedCore/src/commonMain/kotlin/com/wynndie/spwallet/sharedCore/presentation/visualTransformations/AmountVisualTransformation.kt
@@ -3,15 +3,15 @@ package com.wynndie.spwallet.sharedCore.presentation.visualTransformations
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.input.TransformedText
 import androidx.compose.ui.text.input.VisualTransformation
-import com.wynndie.spwallet.sharedCore.presentation.extensions.asFormattedAmount
+import com.wynndie.spwallet.sharedCore.presentation.formatters.asFormattedAmount
 
 internal class AmountVisualTransformation : VisualTransformation {
 
     override fun filter(text: AnnotatedString): TransformedText {
-        val rawText = text.text
-        val formattedText = rawText.asFormattedAmount()
+        val originalText = text.text
+        val formattedText = originalText.asFormattedAmount()
 
-        val offsetMapping = buildOffsetMapping(rawText, formattedText)
+        val offsetMapping = buildOffsetMapping(originalText, formattedText)
 
         return TransformedText(AnnotatedString(formattedText), offsetMapping)
     }
